5f6bfa6584ed74018f6e6ecedd8465f52a5dd377
Lennart Eichhorn <lennarteichhorn@googlemail.com>
2021-09-18 16:23:45 +0200

    Removed unfree vscode extensions

--- shell.nix
+++ shell.nix
@@ -1,26 +1,28 @@
-with import <nixpkgs>{};
-with pkgs;
 let
+  pkgs = import <nixpkgs> {}; 
   rtttl = ps: ps.callPackage ./rtttl.nix {};
-  python = python39.withPackages(ps: with ps; [
+  python = pkgs.python39.withPackages(ps: with ps; [
     (rtttl ps)
     pygobject3
     pyzmq
   ]);
-  vscodium = vscode-with-extensions.override {
+  vscodium = pkgs.vscode-with-extensions.override {
     vscode = pkgs.vscodium;
-    vscodeExtensions = with pkgs.vscode-extensions; [
-      bbenoist.Nix
-      ms-vscode.cpptools
-      ms-python.python
+    vscodeExtensions = [
+      pkgs.vscode-extensions.bbenoist.Nix
     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
       name = "beyond-debug";
       publisher = "coolchyni";
       version = "0.9.6";
       sha256 = "1g5qcblnz67786aaqzszrxx6qc80bylyb87aqlgk6423zv4aksq4";
+    } {
+      name = "vscode-clangd";
+      publisher = "llvm-vs-code-extensions";
+      version = "0.1.13";
+      sha256 = "0qjllvg2wwy3w8gg7q9dkbs0mccxxdq8cg59pbvw0vd8rxn71jpw";
     }];
   };
 in
-mkShell {
-    nativeBuildInputs = [ zeromq python gnumake mbedtls pulseaudio pkgconfig gobjectIntrospection gtk3 gdb vscodium ];
+pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [ zeromq python gnumake mbedtls pulseaudio pkgconfig gobjectIntrospection gtk3 gdb vscodium clang ];
 }
